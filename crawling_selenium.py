b9e8a7c34b57a96d6c871cf0f0914392163c7a34
rusfhr <nsa28961@naver.com>
2024-11-08 00:32:50 +0900

    modified

--- crawling_selenium.py
+++ crawling_selenium.py
@@ -7,27 +7,30 @@ import time
 chrome_driver_path = '/Users/kang-gyeongrok/Desktop/Visual Studio Code/chromedriver'
 
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+# options.add_argument("--headless")  # 브라우저를 표시하지 않고 실행
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
 
+# ChromeDriver 서비스 설정
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service = service, options = options)
 
-url = 'https://n.news.naver.com/mnews/article/005/0001735722'
+url = 'https://news.naver.com'
 
 driver.get(url)
 
 time.sleep(2)
 
-try:
-    title = driver.find_element(By.CLASS_NAME, 'media_end_head_headline').text
-    content = driver.find_element(By.CLASS_NAME, 'go_trans._article_content').text
+driver.find_element(By.XPATH, '/html/body/section/header/div[2]/div/div/div/div/div/ul/li[2]/a').click()
+time.sleep(2)
 
-    print('제목 : ', title)
-    print('\n본문 : ', content)
 
-except Exception as e:
-    print('오류 발생', e)
+
+
+
+
+
+
+
 
 driver.quit()
